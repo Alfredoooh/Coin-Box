ed74cd6201f31f71de23aa6e7f284b2b7aeaaaf3
Alfredoooh <alfredopjonas@gmail.com>
2026-02-05 11:05:08 +0100

    Atualizar o App.tsx

--- coin-box/App.tsx
+++ coin-box/App.tsx
@@ -1,6 +1,6 @@
-// App.tsx
+/* App.tsx
 import React, { useState, useEffect } from 'react';
-import { StatusBar as RNStatusBar, StyleSheet, View, Platform } from 'react-native';
+import { StatusBar as RNStatusBar, StyleSheet, View, Platform } from 'react-native';*/
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { enableScreens } from 'react-native-screens';
