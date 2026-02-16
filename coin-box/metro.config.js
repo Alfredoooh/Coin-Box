7aebd6c9e2aedb9387ae30d4ab9059375422d8a3
Alfredoooh <alfredopjonas@gmail.com>
2026-02-16 13:33:17 +0100

    Atualizar o metro.config.js

--- coin-box/metro.config.js
+++ coin-box/metro.config.js
@@ -1,4 +1,4 @@
-<p>const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
@@ -28,4 +28,4 @@ config.resolver = {
   ],
 };
 
-module.exports = config;</p>
+module.exports = config;
