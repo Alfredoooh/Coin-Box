1adffc05d0a4b8c8dddf5ae92fa30bcc7cfbdd60
Alfredoooh <alfredopjonas@gmail.com>
2026-02-16 13:31:59 +0100

    Excluir babel.config.js

--- coin-box/babel.config.js
+++
@@ -1,8 +0,0 @@
-// babel.config.js
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'], // ou ['module:metro-react-native-babel-preset'] se não usas Expo
-    plugins: ['nativewind/babel'],
-  };
-};
